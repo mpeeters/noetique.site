2c531ef5b536784a2e8b5222ced1d48208f62d09
Sébastien Verbois <sebastien.verbois@unamur.be>
2016-10-29 11:00:55 +0200

    add book faceted-preview-item view

--- src/noetique/site/book/content.py
+++ src/noetique/site/book/content.py
@@ -3,6 +3,7 @@
 from plone.app.textfield import RichText
 from plone.autoform import directives
 from plone.dexterity.content import Item
+from plone.namedfile.field import NamedBlobImage
 from plone.supermodel import model
 from zope import schema
 from zope.interface import implements
@@ -17,47 +18,36 @@ class IBook(model.Schema):
         required=True
     )
     description = schema.Text(
-        title=u"Résumé court du livre",
+        title=u"Bref descriptif du livre",
         required=True,
     )
-    comment = schema.Text(
-        title=u"Commentaire sur le livre",
+    author = schema.TextLine(
+        title=u"Auteur(s)",
+        required=False
+    )
+    cover = NamedBlobImage(
+        title=u"Photo de couverture",
         required=False,
     )
     summary = RichText(
         title=u"Résumé détaillé du livre",
         required=False,
     )
-    author = schema.TextLine(
-        title=u"Auteur(s)",
-        required=False
-    )
-    fourthcover = RichText(
-        title=u"4ème de couverture",
-        required=False,
-    )
     foreword = RichText(
         title=u"Préface du livre",
         required=False,
     )
-    afterward = RichText(
-        title=u"Postface du livre",
-        required=False,
-    )
 
     # widgets
-    directives.widget('description', rows=7)
-    directives.widget('comment', rows=7)
+    directives.widget('description', rows=5)
     directives.widget('summary', rows=15)
-    directives.widget('fourthcover', rows=15)
     directives.widget('foreword', rows=15)
-    directives.widget('afterward', rows=15)
     
     # fieldsets
     model.fieldset(
-        'prepost',
-        label=u"4ème/Pré/Post",
-        fields=['fourthcover', 'foreword', 'afterward']
+        'summary',
+        label=u"Résumé/Préface",
+        fields=['cover', 'summary', 'foreword']
     )
 
 
